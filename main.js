fb5d4885530df8a7d584e076ad143d3271c9e07d
Theo Marchetti Barcelos <barcelostheo5@gmail.com>
2024-11-13 09:27:51 -0300

    ATT
    atualizando js

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 const caixas = document.getElementById("caixa-projeto");
 var flechaBaixo = document.getElementById("down-arrow");
 var flechaCima = document.getElementById("up-arrow");
-var btn = document.getElementsByClassName("botao-flecha")
+var btn = document.getElementById("button-arrow")
 
 function MyFunction(){
-    caixas.style.display = "flex";
-    
-    if (caixas.style.display = "flex"){
+    if (caixas.style.display = "none"){
+        caixas.style.display = "flex";
         flechaCima.style.display = "grid";
         flechaBaixo.style.display = "none";
-    }
-
-    if (caixas.style.display = "flex"){
-        btn.addEventListenter
-    }
+    }   
 }
