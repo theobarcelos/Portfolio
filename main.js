5f7a2ee553dc42ffe2ead5e7da59b1ecc5556f72
Theo Marchetti Barcelos <barcelostheo5@gmail.com>
2024-11-13 08:54:27 -0300

    ATT
    Adicionando Javascript ao código

---
+++ main.js
@@ -0,0 +1,17 @@
+const caixas = document.getElementById("caixa-projeto");
+var flechaBaixo = document.getElementById("down-arrow");
+var flechaCima = document.getElementById("up-arrow");
+var btn = document.getElementsByClassName("botao-flecha")
+
+function MyFunction(){
+    caixas.style.display = "flex";
+    
+    if (caixas.style.display = "flex"){
+        flechaCima.style.display = "grid";
+        flechaBaixo.style.display = "none";
+    }
+
+    if (caixas.style.display = "flex"){
+        btn.addEventListenter
+    }
+}
