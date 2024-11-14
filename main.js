b42f9bbf8a46d03049d16a137e3a225cee1b0c90
Theo Marchetti Barcelos <barcelostheo5@gmail.com>
2024-11-13 22:15:08 -0300

    A

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var flechaCima = document.getElementById("up-arrow");
 var btn = document.getElementById("button-arrow")
 
 function MyFunction(){
-    if (caixas.style.display = "none"){
+    if (flechaBaixo.style.display = "grid"){
         caixas.style.display = "flex";
         flechaCima.style.display = "grid";
         flechaBaixo.style.display = "none";
-    }   
+    }
 }
